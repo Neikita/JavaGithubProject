3b40c5bbf24e26b461e8fe389796eb3171eab702
Nikita <nikita@git.com>
2020-11-24 00:23:03 +0530

    Second Commit - New File and Changes to previous one

--- JAVAPROJECT/src/com/lti/demo/Client.java
+++ JAVAPROJECT/src/com/lti/demo/Client.java
@@ -4,5 +4,7 @@ public class Client {
 public static void main(String[] args) {
 	Person p=new Person("Jay",101,90.00);
 	System.out.println(p);
+	Person p1=new Person("Vijay",101,90.00);
+	System.out.println(p1);
 }
 }
